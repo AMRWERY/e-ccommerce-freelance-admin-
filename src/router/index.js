809589b4557d38d0b526aea9b9dafd3b59cf3146
AMRWERY <amrmounir2@gmail.com>
2025-03-08 00:10:28 +0200

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import i18n from "@/plugins/i18n";
 import home from "../views/home.vue";
 import mainLayout from "@/components/layouts-components/main-layout.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
