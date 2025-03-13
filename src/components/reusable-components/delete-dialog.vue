90b657c4d7886e5627006c7e0f5aa60f42c513ee
AMRWERY <amrmounir2@gmail.com>
2025-03-13 22:10:37 +0200

    ....

--- src/components/reusable-components/delete-dialog.vue
+++ src/components/reusable-components/delete-dialog.vue
@@ -27,7 +27,7 @@
                             Cancel
                         </button>
                         <button @click="confirmDelete" type="button"
-                            class="px-4 py-2 text-sm text-white bg-red-600 rounded-lg hover:bg-red-700 active:bg-red-600">
+                            class="px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                             Delete
                         </button>
                     </div>
