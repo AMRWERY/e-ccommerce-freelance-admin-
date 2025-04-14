821aec6beb0af9b7649c28a253fa8df039ed34ad
AMRWERY <amrmounir2@gmail.com>
2025-04-14 22:01:20 +0200

    remove add product btn from merchant user. also handle user-menu design

--- src/components/layouts-components/user-menu.vue
+++ src/components/layouts-components/user-menu.vue
@@ -1,38 +1,20 @@
 <template>
     <div>
-        <div v-flowbite>
-            <button type="button" class="flex items-center" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                <div class="relative">
-                    <img v-if="userRole.role === 'market_owner' && userRole.imageUrl" :src="userRole.imageUrl"
-                        class="object-cover w-10 h-10 rounded-full" alt="merchant-avatar">
-                    <img v-else src="https://justfields.com/storage/projects/7M5rV059/images.jpg"
-                        class="object-cover w-10 h-10 rounded-full" alt="user-avatar">
-                    <div class="absolute inset-0 rounded-full shadow-inner"></div>
-                </div>
-                <div class="ms-4">
-                    <h2 class="font-bold text-gray-200 text-md">
-                        {{ userRole.role === 'market_owner' ? userRole.name : userRole.role }}
-                    </h2>
-                    <p class="text-sm text-gray-100">{{ userRole.email }}</p>
-                </div>
-            </button>
-            <div class="z-50 hidden w-40 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm dark:bg-gray-700 dark:divide-gray-600"
-                id="dropdown-user">
-                <ul class="py-1" role="none">
-                    <li
-                        v-if="userRole && (userRole.role === 'admin' || userRole.role === 'employee' || userRole.role === 'market_owner')">
-                        <router-link to="/profile"
-                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                            role="menuitem">{{ $t('profile_menu.profile') }}</router-link>
-                    </li>
-                    <li v-if="userRole && userRole.role === 'admin'">
-                        <router-link to="/earnings"
-                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                            role="menuitem">{{ $t('profile_menu.earnings') }}</router-link>
-                    </li>
-                </ul>
+        <router-link to="/profile" type="button" class="flex items-center">
+            <div class="relative">
+                <img v-if="userRole.role === 'market_owner' && userRole.imageUrl" :src="userRole.imageUrl"
+                    class="object-cover w-10 h-10 rounded-full" alt="merchant-avatar">
+                <img v-else src="https://justfields.com/storage/projects/7M5rV059/images.jpg"
+                    class="object-cover w-10 h-10 rounded-full" alt="user-avatar">
+                <div class="absolute inset-0 rounded-full shadow-inner"></div>
             </div>
-        </div>
+            <div class="ms-4">
+                <h2 class="font-bold text-gray-200 text-md">
+                    {{ userRole.role === 'market_owner' ? userRole.name : userRole.role }}
+                </h2>
+                <p class="text-sm text-gray-100">{{ userRole.email }}</p>
+            </div>
+        </router-link>
     </div>
 </template>
 
--- src/views/products-stocks/index.vue
+++ src/views/products-stocks/index.vue
@@ -9,7 +9,7 @@
                         <excel-export-btn :export-handler="handleExport" />
                     </div>
 
-                    <router-link to="" role="button" @click="openAddProductDialog"
+                    <router-link to="" role="button" @click="openAddProductDialog" v-if="userRole?.role !== 'market_owner'"
                         class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2">
                         <iconify-icon icon="ic:baseline-plus" width="24" height="24"></iconify-icon>
                         {{ $t('btn.add_product') }}
