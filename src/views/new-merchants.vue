130c6251ac99dc868991d17eff8b116b54dff987
AMRWERY <amrmounir2@gmail.com>
2025-04-15 17:30:25 +0200

    handle new-merchants translations and skeleton loader

--- src/views/new-merchants.vue
+++ src/views/new-merchants.vue
@@ -2,132 +2,128 @@
     <div>
         <div v-flowbite>
             <div class="flex items-center justify-between my-10 flex-nowrap">
-                <p class="text-3xl font-semibold text-gray-700">Merchants</p>
-                <div class="flex items-center justify-center gap-4">
+                <p class="text-3xl font-semibold text-gray-700">{{ $t('dashboard.merchants') }}</p>
+                <div class="flex items-center justify-center gap-4" v-if="!showSkeleton">
                     <select v-model="newMerchantStore.selectedStatus"
                         @change="newMerchantStore.setStatusFilter(newMerchantStore.selectedStatus)"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 min-w-48">
-                        <option value="all">All Status</option>
-                        <option value="pending">Pending</option>
-                        <option value="approved">Approved</option>
-                        <option value="rejected">Rejected</option>
+                        <option value="all">{{ $t('dashboard.all_status') }}</option>
+                        <option value="pending">{{ $t('dashboard.pending') }}</option>
+                        <option value="approved">{{ $t('dashboard.approved') }}</option>
+                        <option value="rejected">{{ $t('dashboard.rejected') }}</option>
                     </select>
                 </div>
             </div>
+
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-gray-500 text-start">
-                    <thead class="text-xs text-gray-700 capitalize bg-gray-50">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">#</th>
-                            <th scope="col" class="px-6 py-3">Merchant Logo</th>
-                            <th scope="col" class="px-6 py-3">Merchant Name</th>
-                            <th scope="col" class="px-6 py-3">Email</th>
-                            <th scope="col" class="px-6 py-3">{{ $t('dashboard.name') }}</th>
-                            <th scope="col" class="px-6 py-3">Merchant ID</th>
-                            <th scope="col" class="px-6 py-3">Status</th>
-                            <th scope="col" class="px-6 py-3"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="bg-white border-b border-gray-200 hover:bg-gray-50"
-                            v-for="(merchant, index) in newMerchantStore.paginatedMerchants" :key="merchant.id">
-                            <td class="w-4 p-4">
-                                <div class="flex items-center">
-                                    {{ (newMerchantStore.currentPage - 1) * newMerchantStore.merchantsPerPage + index +
-                                        1 }}
-                                </div>
-                            </td>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                <img :src="merchant.imageUrl || '/default-market.png'" alt="merchant-logo"
-                                    class="object-cover w-12 h-12 rounded-lg">
-                            </th>
-                            <td class="px-6 py-4">{{ merchant.name }}</td>
-                            <td class="px-6 py-4">{{ merchant.email }}</td>
-                            <td class="px-6 py-4">{{ merchant.firstName }} {{ merchant.lastName }}</td>
-                            <td class="px-6 py-4">{{ merchant.marketId }}</td>
-                            <td class="px-6 py-4">
-                                <span :class="{
-                                    'px-2.5 py-0.5 rounded-full text-xs font-medium': true,
-                                    'bg-yellow-100 text-yellow-800': merchant.status === 'pending',
-                                    'bg-green-100 text-green-800': merchant.status === 'approved',
-                                    'bg-red-100 text-red-800': merchant.status === 'rejected'
-                                }">
-                                    {{ merchant.status }}
-                                </span>
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="flex gap-3" v-if="merchant.status === 'pending'">
-                                    <button @click="handleAccept(merchant.id)"
-                                        class="font-medium text-blue-600 hover:underline disabled:opacity-50 disabled:cursor-not-allowed"
+                <template v-if="showSkeleton">
+                    <!-- tableSkeletonLoader component -->
+                    <table-skeleton-loader :headers="skeletonHeaders" :rows="5" />
+                </template>
+
+                <template v-else>
+                    <table class="w-full text-sm text-gray-500 text-start">
+                        <thead class="text-xs text-gray-700 capitalize bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">#</th>
+                                <th scope="col" class="px-6 py-3">{{ $t('dashboard.merchant_logo') }}</th>
+                                <th scope="col" class="px-6 py-3">{{ $t('dashboard.merchant_name') }}</th>
+                                <th scope="col" class="px-6 py-3">{{ $t('dashboard.email') }}</th>
+                                <th scope="col" class="px-6 py-3">{{ $t('dashboard.name') }}</th>
+                                <th scope="col" class="px-6 py-3">{{ $t('dashboard.merchant_id') }}</th>
+                                <th scope="col" class="px-6 py-3">{{ $t('dashboard.status') }}</th>
+                                <th scope="col" class="px-6 py-3"></th>
+                            </tr>
+                        </thead>
+
+                        <tbody v-if="newMerchantStore.paginatedMerchants === 0">
+                            <tr>
+                                <td colspan="10" class="p-4 text-center">
+                                    <!-- NoDataMessage component -->
+                                    <No-data-message :message="$t('no_data.no_merchants_found')"
+                                        icon="mdi:shop-alert" />
+                                </td>
+                            </tr>
+                        </tbody>
+
+                        <tbody>
+                            <tr class="bg-white border-b border-gray-200 hover:bg-gray-50"
+                                v-for="(merchant, index) in newMerchantStore.paginatedMerchants" :key="merchant.id">
+                                <td class="w-4 p-4">
+                                    <div class="flex items-center">
+                                        {{ (newMerchantStore.currentPage - 1) * newMerchantStore.merchantsPerPage +
+                                            index +
+                                            1 }}
+                                    </div>
+                                </td>
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    <img :src="merchant.imageUrl || '/default-market.png'" alt="merchant-logo"
+                                        class="object-cover w-12 h-12 rounded-lg">
+                                </th>
+                                <td class="px-6 py-4">{{ merchant.name }}</td>
+                                <td class="px-6 py-4">{{ merchant.email }}</td>
+                                <td class="px-6 py-4">{{ merchant.firstName }} {{ merchant.lastName }}</td>
+                                <td class="px-6 py-4">{{ merchant.marketId }}</td>
+                                <td class="px-6 py-4">
+                                    <span :class="{
+                                        'px-2.5 py-0.5 rounded-full text-xs font-medium': true,
+                                        'bg-yellow-100 text-yellow-800': merchant.status === 'pending',
+                                        'bg-green-100 text-green-800': merchant.status === 'approved',
+                                        'bg-red-100 text-red-800': merchant.status === 'rejected'
+                                    }">
+                                        {{ merchant.status }}
+                                    </span>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="flex gap-3" v-if="merchant.status === 'pending'">
+                                        <button @click="handleAccept(merchant.id)"
+                                            class="font-medium text-blue-600 hover:underline disabled:opacity-50 disabled:cursor-not-allowed"
+                                            :disabled="loadingMerchant === merchant.id">
+                                            <span v-if="loadingMerchant === merchant.id && loadingAction === 'accept'"
+                                                class="flex items-center">
+                                                <iconify-icon icon="line-md:loading-loop" width="20" height="20"
+                                                    class="me-1"></iconify-icon>
+                                            </span>
+                                            <span v-else>{{ $t('btn.accept') }}</span>
+                                        </button>
+                                        <button @click="handleReject(merchant.id)"
+                                            class="font-medium text-red-600 hover:underline disabled:opacity-50 disabled:cursor-not-allowed"
+                                            :disabled="loadingMerchant === merchant.id">
+                                            <span v-if="loadingMerchant === merchant.id && loadingAction === 'reject'"
+                                                class="flex items-center">
+                                                <iconify-icon icon="line-md:loading-loop" width="20" height="20"
+                                                    class="me-1"></iconify-icon>
+                                            </span>
+                                            <span v-else>{{ $t('btn.reject') }}</span>
+                                        </button>
+                                    </div>
+                                    <button v-else-if="merchant.status === 'rejected'"
+                                        @click="openDeleteDialog(merchant.id)"
+                                        class="flex items-center font-medium text-red-600 hover:text-red-800 disabled:opacity-50 disabled:cursor-not-allowed"
                                         :disabled="loadingMerchant === merchant.id">
-                                        <span v-if="loadingMerchant === merchant.id && loadingAction === 'accept'"
+                                        <span v-if="loadingMerchant === merchant.id && loadingAction === 'delete'"
                                             class="flex items-center">
                                             <iconify-icon icon="line-md:loading-loop" width="20" height="20"
                                                 class="me-1"></iconify-icon>
-                                            Accepting...
                                         </span>
-                                        <span v-else>Accept</span>
-                                    </button>
-                                    <button @click="handleReject(merchant.id)"
-                                        class="font-medium text-red-600 hover:underline disabled:opacity-50 disabled:cursor-not-allowed"
-                                        :disabled="loadingMerchant === merchant.id">
-                                        <span v-if="loadingMerchant === merchant.id && loadingAction === 'reject'"
-                                            class="flex items-center">
-                                            <iconify-icon icon="line-md:loading-loop" width="20" height="20"
+                                        <span v-else class="flex items-center">
+                                            <iconify-icon icon="material-symbols:delete" width="20" height="20"
                                                 class="me-1"></iconify-icon>
-                                            Rejecting...
+                                            {{ $t('btn.delete') }}
                                         </span>
-                                        <span v-else>Reject</span>
                                     </button>
-                                </div>
-                                <button v-else-if="merchant.status === 'rejected'"
-                                    @click="openDeleteDialog(merchant.id)"
-                                    class="flex items-center font-medium text-red-600 hover:text-red-800 disabled:opacity-50 disabled:cursor-not-allowed"
-                                    :disabled="loadingMerchant === merchant.id">
-                                    <span v-if="loadingMerchant === merchant.id && loadingAction === 'delete'"
-                                        class="flex items-center">
-                                        <iconify-icon icon="line-md:loading-loop" width="20" height="20"
-                                            class="me-1"></iconify-icon>
-                                    </span>
-                                    <span v-else class="flex items-center">
-                                        <iconify-icon icon="material-symbols:delete" width="20" height="20"
-                                            class="me-1"></iconify-icon>
-                                        {{ $t('btn.delete') }}
-                                    </span>
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </template>
             </div>
 
             <!-- pagination -->
-            <div class="flex items-end justify-end mt-12">
-                <div class="flex items-center px-4 py-3" v-if="newMerchantStore.paginatedMerchants.length > 0">
-                    <div class="flex mt-3 space-s-1 ms-auto">
-                        <button role="button" @click="newMerchantStore.changePage(newMerchantStore.currentPage - 1)"
-                            :disabled="newMerchantStore.currentPage === 1"
-                            class="flex items-center justify-center bg-gray-100 rounded-md shrink-0 w-9 h-9 disabled:opacity-50">
-                            <iconify-icon icon="material-symbols:keyboard-arrow-left" width="24" height="24"
-                                class="text-gray-400 rtl:rotate-180"></iconify-icon>
-                        </button>
-                        <button v-for="page in newMerchantStore.totalPages" :key="page"
-                            @click="newMerchantStore.changePage(page)" :class="{
-                                'bg-slate-300 text-gray-800': page === newMerchantStore.currentPage,
-                                'bg-slate-100 text-gray-600': page !== newMerchantStore.currentPage,
-                            }"
-                            class="flex items-center justify-center shrink-0 border hover:border-blue-500 cursor-pointer text-base font-medium text-spate-900 px-[13px] h-9 rounded-md">
-                            {{ page }}
-                        </button>
-                        <button role="button" @click="newMerchantStore.changePage(newMerchantStore.currentPage + 1)"
-                            :disabled="newMerchantStore.currentPage === newMerchantStore.totalPages"
-                            class="flex items-center justify-center border rounded-md cursor-pointer shrink-0 hover:border-blue-500 w-9 h-9 disabled:opacity-50">
-                            <iconify-icon icon="material-symbols:keyboard-arrow-right" width="24" height="24"
-                                class="text-gray-400 rtl:rotate-180"></iconify-icon>
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <!-- paginationControls component -->
+            <pagination-controls v-if="!showSkeleton && newMerchantStore.paginatedMerchants.length > 0"
+                :current-page="newMerchantStore.currentPage" :total-pages="newMerchantStore.totalPages"
+                @page-change="newMerchantStore.changePage" />
         </div>
 
         <!-- delete-dialog component -->
@@ -154,6 +150,7 @@ const loadingAction = ref(null);
 const showDeleteDialog = ref(false);
 const selectedMerchantId = ref(null);
 const { showToast, toastMessage, toastType, toastIcon, triggerToast } = useToast();
+const showSkeleton = ref(true);
 
 const handleAccept = async (merchantId) => {
     loadingMerchant.value = merchantId;
@@ -211,7 +208,26 @@ const handleDelete = async () => {
     }
 };
 
-onMounted(() => {
-    newMerchantStore.fetchAllMerchants();
+onMounted(async () => {
+    const startTime = Date.now();
+    try {
+        await newMerchantStore.fetchAllMerchants();
+    } finally {
+        const elapsed = Date.now() - startTime;
+        const remaining = Math.max(3000 - elapsed, 0);
+        setTimeout(() => {
+            showSkeleton.value = false;
+        }, remaining);
+    }
 });
+
+const skeletonHeaders = [
+    { label: '#', loaderWidth: 'w-8' },
+    { label: 'Merchant Logo', type: 'image' },
+    { label: 'Merchant Name', loaderWidth: 'w-32' },
+    { label: 'Email', loaderWidth: 'w-24' },
+    { label: 'Name', loaderWidth: 'w-24' },
+    { label: 'Merchant ID', loaderWidth: 'w-24' },
+    { label: 'Status', loaderWidth: 'w-24' },
+];
 </script>
